cef746321af289cad8f72c6617f16155c08b1e1a
akzk <leonardocaesarz@gmail.com>
2018-01-04 01:29:20 +0800

    update algorithm

--- wechat_jump.py
+++ wechat_jump.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
 import time
+import random
 
+coef1 = 1.48
+coef2 = 1.43
+coef3 = 1.39
+
+coef = coef1
 
 def get_point_with_diff_color(img, start_x, start_y, end_x, end_y, allow_range=0.1, ignore_x=(), ignore_y=()):
 
@@ -37,34 +43,13 @@ def find_target_top_point(img, start_point_x, start_y=500, allow_range=0.1):
         start_x = 100
         end_x = start_point_x - 50
 
-    return get_point_with_diff_color(img, start_x, start_y, end_x, img.shape[0], ignore_x=(start_point_x-60, start_point_x+60))
+    return get_point_with_diff_color(img, start_x, start_y, end_x, img.shape[0], ignore_x=(start_point_x-100, start_point_x+100))
 
 def find_target_bottom_point(img, top_point):
 
     return get_point_with_diff_color(img, top_point[1], top_point[0]+1, top_point[1]+1, img.shape[0])
 
-def get_target_point(img, start_point_x):
-
-    top_point = find_target_top_point(img, start_point_x)
-    if not top_point:
-        return False
-
-    bottom_point = find_target_bottom_point(img, top_point)
-    if not bottom_point:
-        return False
-
-    h = bottom_point[0] - top_point[0]
-    if h < 30:
-        h = 25
-    elif h > 300:
-        h = 25
-    else:
-        h /= 2
-
-    return (int(top_point[0]+h), top_point[1])
-
-def find_start_point(img, y_start=1000, y_end=1500):
-
+def find_chess_point(img, y_start, y_end):
     min_color=[0.20, 0.22, 0.38]
     max_color=[0.22, 0.24, 0.42]
     
@@ -78,10 +63,17 @@ def find_start_point(img, y_start=1000, y_end=1500):
                 if min_color[i] > color[i] or color[i] > max_color[i]:
                     isit = False
                     break
-            if isit:    
+            if isit:
                 return (y-20, x+10)
     
     return False
+
+def find_start_point(img):
+    return find_chess_point(img, 1000, 1500)
+
+def find_actual_drop_point(img, target_y):
+    return find_chess_point(img, target_y-100, target_y+100)
+    
 
 def get_screenshot():
 
@@ -94,14 +86,12 @@ def get_screenshot():
 
 def jump(distance):
 
-    a = 1.45
-
-    press_time = distance * a
+    press_time = distance * coef
     press_time = max(press_time, 100)
     press_time = int(press_time)
 
     print("s: %d pixel, t: %d ms" % (distance, press_time))
-    os.system("adb shell input swipe 500 1600 500 1600 {duration}".format(duration=press_time))
+    os.system("adb shell input swipe {x} {y} {x} {y} {duration}".format(duration=press_time, x=int(random.uniform(0, 300))+300, y=int(random.uniform(0, 300))+900))
 
 def isEnd(img):
     
@@ -138,13 +128,12 @@ while 1:
     img = mpimg.imread("/Users/leonardo/Desktop/tmp.png")
 
     if isEnd(img):
+        name = time.strftime('%Y%m%d-%H%M',time.localtime(time.time()))
         if os.path.exists("/Users/leonardo/Desktop/tmp2.png"):
-            name = time.strftime('%Y%m%d-%H%M.png',time.localtime(time.time()))
-            os.rename("/Users/leonardo/Desktop/tmp2.png", "/Users/leonardo/Desktop/jump/"+name+"-a")
+            os.rename("/Users/leonardo/Desktop/tmp2.png", "/Users/leonardo/Desktop/jump/"+name+"-b.png")
         
-        if os.path.exists("/Users/leonardo/Desktop/tmp2.png"):
-            name = time.strftime('%Y%m%d-%H%M.png',time.localtime(time.time()))
-            os.rename("/Users/leonardo/Desktop/tmp2.png", "/Users/leonardo/Desktop/jump/"+name+"-b")
+        if os.path.exists("/Users/leonardo/Desktop/tmp3.png"):
+            os.rename("/Users/leonardo/Desktop/tmp3.png", "/Users/leonardo/Desktop/jump/"+name+"-a.png")
 
         os.system("adb shell input tap 500 1600")
         time.sleep(1)
@@ -157,24 +146,51 @@ while 1:
 
     print("start point:", start_point)
 
-    target_point = get_target_point(img, start_point[1])
-    if not target_point:
+    target_top_point = find_target_top_point(img, start_point[1])
+    if not target_top_point:
         break
+
+    x_distance = abs(start_point[1]-target_top_point[1])
+    y_distance = 0.685 * x_distance
+
+    target_point = (int(start_point[0]-y_distance), target_top_point[1])
+
+    if target_point[0]-target_top_point[0] < 10:
+        x_distance -= 20
+        y_distance = 0.685 * x_distance
+        target_point = (int(start_point[0]-y_distance), target_top_point[1])
 
     print("target point:", target_point)
 
-    # plt.imshow(img)
-    # plt.plot([start_point[1], target_point[1]], [start_point[0], target_point[0]])
-    # plt.savefig("/Users/leonardo/Desktop/tmp.png")
-    # exit()
-
-    x_distance = abs(start_point[0]-target_point[0])
-    y_distance = abs(start_point[1]-target_point[1])
+    coef = coef1
+    if 450 > x_distance > 250:
+        coef = coef2
+    elif x_distance >= 450:
+        coef = coef3
 
     distance = math.sqrt(x_distance**2 + y_distance**2)
     print("x_d: %d pixel, y_d: %d pixel" % (x_distance, y_distance))
 
     jump(distance)
-    time.sleep(3)
+
+    time.sleep(0.3)
+    os.remove("/Users/leonardo/Desktop/tmp.png")
+    get_screenshot()
+    img = mpimg.imread("/Users/leonardo/Desktop/tmp.png")
+    actual_point = find_actual_drop_point(img, target_point[0])
+    if not actual_point:
+        time.sleep(3)
+        continue
+    
+    plt.clf()
+    plt.imshow(img)
+    plt.plot(actual_point[1], actual_point[0], 'o')
+    plt.plot([start_point[1], target_point[1]], [start_point[0], target_point[0]], color="red")
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis("off")
+    plt.savefig("/Users/leonardo/Desktop/tmp.png", bbox_inches="tight")
+
+    time.sleep(3+int(random.uniform(0, 3)))
 
 
